1bb44b8cb04911567c4578e7d91dbadf93a6d483
Suyash Purwar <purwarsuyash06@gmail.com>
2023-01-29 18:15:12 +0530

    Add middleware for user authentication

--- router/bot.router.js
+++ router/bot.router.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import * as botController from './../controllers/bot.controller.js';
+import authenticate from '../middlewares/authenticate.js';
 
 const router = Router();
 
 router.get('/ping', (req, res) => res.send('pong'));
-router.post('/process-msg', botController.processMsg);
+router.post('/process-msg', authenticate, botController.processMsg);
 
 export default router;
